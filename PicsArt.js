79e55a4ac011a637ebf2f741e3de78c7367b58e7
𝐐𝐮𝐨̂́𝐜 𝐂𝐡𝐢𝐞̂́𝐧  <hackiosipa@gmail.com>
2026-09-02 13:12:28 +0700

    Update PicsArt.js

--- PicsArt.js
+++ PicsArt.js
@@ -1,38 +1,36 @@
-$done({
-    body: JSON.stringify({
-        "status": "success",
-        "response": [
-            {
-                "status": "SUBSCRIPTION_PURCHASED",
-                "order_id": "490001314520000",
-                "original_order_id": "490001314520000",
-                "is_trial": false,
-                "plan_meta": {
-                    "storage_limit_in_mb": 20480,
-                    "frequency": "yearly",
-                    "scope_id": "full",
-                    "id": "com.picsart.editor.subscription_yearly",
-                    "product_id": "subscription_yearly",
-                    "level": 2000,
-                    "auto_renew_product_id": "com.picsart.editor.subscription_yearly",
-                    "type": "renewable",
-                    "tier_id": "gold_old",
-                    "permissions": [
-                        "premium_tools_standard",
-                        "premium_tools_ai"
-                    ],
-                    "description": "china"
-                },
-                "limitation": {
-                    "max_count": 5,
-                    "limits_exceeded": false
-                },
-                "reason": "ok",
-                "subscription_id": "com.picsart.editor.subscription_yearly",
-                "is_eligible_for_introductory": false,
-                "purchase_date": Date.now(),
-                "expire_date": Date.now() + 315360000000
-            }
-        ]
-    })
-});
+let objc = {
+  "status": "success",
+    "response": [
+        {
+            "status": "SUBSCRIPTION_RENEWED",
+            "is_trial": false,
+            "order_id": "2000001108782321",
+            "expire_date": 32662137600000,
+            "purchase_date": 1704758400000,
+            "subscription_id": "com.picsart.studio.subscription_pro_3_yearly",
+            "original_order_id": "2000000756285050",
+            "plan_meta": {
+                "permissions": [
+                    "premium_tools_standard",
+                    "premium_tools_ai"
+                ],
+                "auto_renew_product_id": "com.picsart.studio.subscription_mac_tier1_pro_yearly",
+                "level": 2000,
+                "storage_limit_in_mb": 102400,
+                "id": "com.picsart.studio.subscription_pro_3_yearly",
+                "frequency": "yearly",
+                "type": "renewable",
+                "scope_id": "full",
+                "product_id": "com.picsart.studio.subscription_pro_3_yearly",
+                "tier_id": "pro"
+            },
+            "limitation": {
+                "max_count": 10,
+                "limits_exceeded": false
+            },
+            "is_eligible_for_introductory": false,
+            "reason": "ok"
+        }
+    ]
+}
+$done({ response: {body: JSON.stringify(objc),status: 200} });
